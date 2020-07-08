9ca4ba1b7f04c594bc6eddcc70a1b2efe47120b2
Tafadzwa Joseph Dube <t.j.dube89@gmail.com>
2020-07-07 17:06:01 -0700

    Updates

---
+++ precache-manifest.05667d3c70fa13ec0d5d24ef233fd82d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891f2fb137d9d623224d25eccce4f55a",
+    "url": "/eshopfront/index.html"
+  },
+  {
+    "revision": "6b6dbd8460ac1fe218a0",
+    "url": "/eshopfront/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "68b3b0d0331569f32377",
+    "url": "/eshopfront/static/js/2.84bdfa0d.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/eshopfront/static/js/2.84bdfa0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b6dbd8460ac1fe218a0",
+    "url": "/eshopfront/static/js/main.fb01ed3f.chunk.js"
+  },
+  {
+    "revision": "8ad65de691b51c44d0e8",
+    "url": "/eshopfront/static/js/runtime-main.c258b9bf.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/eshopfront/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/eshopfront/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/eshopfront/static/media/logo.5d5d9eef.svg"
+  }
+]);
